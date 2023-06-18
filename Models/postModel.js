45daac681f53857c9b143332cb527993d2f7cdd4
parvesh <vparvesh@gmail.com>
2023-06-18 12:33:55 +0530

    indexed post category field

--- Models/postModel.js
+++ Models/postModel.js
@@ -1,43 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const blogPostSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-    maxLenght:[20, "Title must be under 20 words limit"]
+const blogPostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      maxLenght: [20, "Title must be under 20 words limit"],
+    },
+    content: {
+      type: String,
+      required: [true, "Content is required"],
+      maxLenght: [500, "Content must be under 500 words limit"],
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+      required: [true, "Author is required"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Author",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Author",
+      },
+    ],
+    views: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Author",
+      },
+    ],
+    image: {
+      type: String,
+      required: [true, "Image is required"],
+    },
   },
-  content: {
-    type: String,
-    required: [true, "Content is required"],
-    maxLenght:[500,"Content must be under 500 words limit"]
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Author',
-    required: [true, "Author is required"]
-  },
-  category:{
-   type:String,
-   required:[true, "Category is required"]
-  },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Author'
-  }],
-  dislikes:  [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Author'
-  }],
-  views:  [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Author'
-  }],
-  image: {
-    type: String,
-    required: [true, "Image is required"]
-  }
-}, {timestamps:true});
+  { timestamps: true }
+);
 
-const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+blogPostSchema.index({ category: 1 });
+
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
 
 module.exports = BlogPost;
